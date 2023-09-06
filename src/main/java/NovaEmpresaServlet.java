379fe2105c8a732a4592efd20643d15d700cdac0
Klinger Marinho <klingercm@hotmail.com>
2023-09-06 16:27:59 -0400

    Servelet somente post

--- src/main/java/NovaEmpresaServlet.java
+++ src/main/java/NovaEmpresaServlet.java
@@ -11,13 +11,11 @@ import java.io.PrintWriter;
 /**
  * Servlet implementation class NovaEmpresaServlet
  */
+@WebServlet("/novaEmpresa")
 public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		 System.out.println("Cadastrando nova empresa");
 	        String nomeEmpresa = request.getParameter("nome");
